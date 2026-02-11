db4bff162dce8cc79bf4aa9ab16609073d6106c8
mibana <ksy661004@gmail.com>
2026-02-11 17:53:15 +0900

    [Fix] 공휴일 api의 삭제
    공휴일 api를 사용하면 5년간의 공휴일만 볼 수 있어서 삭제

--- Assets/_Scripts/Calendar/HolidayDatabase.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Calendar/Holiday Database")]
-public class HolidayDatabase : ScriptableObject
-{
-    public List<int> dates = new List<int>();
-}
