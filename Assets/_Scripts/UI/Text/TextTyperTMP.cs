cb34a275798b6879df5780ec130ac3afef2e0a55
kether00 <jein0318@gmail.com>
2026-02-25 20:04:07 +0900

    [feat]체팅 오토 추가
    그 채팅 따다닥 올라오는기능

---
+++ Assets/_Scripts/UI/Text/TextTyperTMP.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+using System.Collections;
+
+public class TextTyperTMP : MonoBehaviour
+{
+    [SerializeField] private TMP_Text tmpText;
+    [SerializeField] private float charactersPerSecond = 30f;
+
+    private Coroutine typingCoroutine;
+    private bool isTyping;
+
+    public void Play(string message)
+    {
+        if (typingCoroutine != null)
+            StopCoroutine(typingCoroutine);
+
+        tmpText.text = message;
+        tmpText.ForceMeshUpdate();
+        tmpText.maxVisibleCharacters = 0;
+
+        typingCoroutine = StartCoroutine(TypeRoutine());
+    }
+
+    private IEnumerator TypeRoutine()
+    {
+        isTyping = true;
+
+        int totalChars = tmpText.textInfo.characterCount;
+        float delay = 1f / charactersPerSecond;
+
+        for (int i = 0; i <= totalChars; i++)
+        {
+            tmpText.maxVisibleCharacters = i;
+            yield return new WaitForSeconds(delay);
+        }
+
+        isTyping = false;
+    }
+
+    public void Skip()
+    {
+        if (!isTyping) return;
+
+        StopCoroutine(typingCoroutine);
+        tmpText.maxVisibleCharacters = tmpText.textInfo.characterCount;
+        isTyping = false;
+    }
+}
